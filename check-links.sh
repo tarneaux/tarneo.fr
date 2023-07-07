ac1e8c9824e0c0c481e47f92e7c545c530ef6a8a
tarneo <tarneo@tarneo.fr>
2023-07-07 21:47:14 +0200

    Allow link check to pass if we time out
    If we time out, the remote server is probably down and it's not our fault.
    Before this commit, the check would fail, preventing us from deploying the
    site. Now, the check passes and just prints a warning, still exiting with exit
    code 0 if there are no other errors.
    
    note: this relies on curl printing HTTP code 000 on timeout, and there may be
    some other reasons for 000.

--- check-links.sh
+++ check-links.sh
@@ -3,11 +3,15 @@
 links=$(find content/ -type f -name "*.md" -exec cat '{}' \; | grep -oE "https?://[a-zA-Z0-9./?=_-]*")
 
 check_url() {
-	http_code=$(curl -s -o /dev/null -w "%{http_code}" "$line")
+	http_code=$(curl -s -m 5 -o /dev/null -w "%{http_code}" "$line")
 	if [ "$http_code" -eq 200 ]; then
 		echo -e "\e[32m[OK]\e[0m $line"
 	elif [ "$http_code" -eq 301 ] || [ "$http_code" -eq 302 ]; then
 		echo -e "\e[33m[REDIRECT]\e[0m $line -> $(curl -s -o /dev/null -w "%{redirect_url}" "$line")"
+	elif [ "$http_code" -eq 000 ]; then
+		# We probably timed out. Maybe the site is down?
+		# Anyway, we'll print a warning and continue
+		echo -e "\e[33m[WARN]\e[0m $line timed out"
 	else
 		echo -e "\e[31m[FAIL]\e[0m $line: $http_code"
 	fi
@@ -23,6 +27,11 @@ out=$(
 ok=$(echo "$out" | grep -c "OK")
 redir=$(echo "$out" | grep -c "REDIRECT")
 fail=$(echo "$out" | grep -c "FAIL")
+warn=$(echo "$out" | grep -c "WARN")
+
+if [ "$warn" -gt 0 ]; then
+	echo -e "\e[33m[WARN]\e[0m $warn links timed out"
+fi
 
 wait
 
