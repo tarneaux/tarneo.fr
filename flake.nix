8a612404da96adb3398bf31942ac46981ed49506
tarneo <tarneo@tarneo.fr>
2024-12-19 18:42:29 +0100

    flake: Bump nixpkgs 23.11 -> 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "tarneo.fr";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
